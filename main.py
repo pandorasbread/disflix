2b1cce02af8a35640546f84f4703ea0ace88cb4c
pandorasbread <pandorasbread@gmaail.com>
2024-05-21 08:12:30 -0600

    movie bot

---
+++ main.py
@@ -0,0 +1,15 @@
+from chatclient import start_bot
+import asyncio
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+async def main():
+    print('starting bot.')
+    await start_bot()
+
+asyncio.run(main())
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
